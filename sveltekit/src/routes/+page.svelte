f907c1d15b020bd701fe3c6da76763266cdeb113
Stephane Demotte <stephanedemotte@mac-3.local>
2025-03-07 06:13:43 -0500

    it's working...

--- sveltekit/src/routes/+page.svelte
+++ sveltekit/src/routes/+page.svelte
@@ -1,9 +1,13 @@
 <script>
   import { page } from "$app/state";
   const form = page.data.entry.form[0];
+
+  console.log(form.templateJs);
 </script>
 
 <div class="w-1/2 mx-auto mt-40">
   {@html form.templateHtml}
-  {@html form.templateJS}
 </div>
+<svelte:head>
+  {@html form.templateJs}
+</svelte:head>
